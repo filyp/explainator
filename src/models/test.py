a3767492d4b3a5f18cd16df82f61b1546a019578
Anna <anna.belowska2@gmail.com>
2019-10-27 16:16:21 +0100

    Implement prediction using pre-trained model from tfhub

--- src/models/test.py
+++ src/models/test.py
@@ -1,12 +1,30 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+import numpy as np
 
-
-hub_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_035_128/classification/4"
+SIZE = 224
+hub_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_035_224/classification/4"
 embed = hub.KerasLayer(hub_url)
 model = tf.keras.Sequential([
     embed
 ])
 
-model.build([None, 128, 128, 3])  # Batch input shape.
+model.build([None, SIZE, SIZE, 3])  # Batch input shape.
 model.summary()
+
+file_path = '/Users/Anna/Explainator/explainator/data/gp.jpeg'
+
+img = tf.io.read_file(file_path)
+img = tf.image.decode_jpeg(img)
+
+images = tf.expand_dims(img, 0)
+images = tf.cast(images, tf.float32) / 224.
+images.set_shape((None, None, None, 3))
+test_image = tf.image.resize(images, (SIZE, SIZE))
+
+
+predictions = model.predict(test_image)
+
+pred = np.argmax(predictions[0])
+
+print(pred)
