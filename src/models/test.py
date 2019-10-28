985f4f24a09fab2e5b05777a9045b9ce6f391756
Anna <anna.belowska2@gmail.com>
2019-10-28 12:51:17 +0100

    Added file with classes

--- src/models/test.py
+++ src/models/test.py
@@ -12,13 +12,13 @@ model = tf.keras.Sequential([
 model.build([None, SIZE, SIZE, 3])  # Batch input shape.
 model.summary()
 
-file_path = '/Users/Anna/Explainator/explainator/data/gp.jpeg'
+file_path = '/Users/Anna/Explainator/explainator/data/panda.jpg'
 
 img = tf.io.read_file(file_path)
 img = tf.image.decode_jpeg(img)
 
 images = tf.expand_dims(img, 0)
-images = tf.cast(images, tf.float32) / 224.
+images = tf.cast(images, tf.float32) / 128.
 images.set_shape((None, None, None, 3))
 test_image = tf.image.resize(images, (SIZE, SIZE))
 
