2f335d69477b083d3704f4da5dedd9c18d5ec892
Anna <anna.belowska2@gmail.com>
2019-11-03 00:23:05 +0100

    Displaying predicted image class name with score

--- src/models/test.py
+++ src/models/test.py
@@ -3,15 +3,24 @@ import tensorflow_hub as hub
 import numpy as np
 
 SIZE = 224
+CLASSES_FILENAME = '/Users/Anna/Explainator/explainator/data/classes.txt'
+
+# download construct model
 hub_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_035_224/classification/4"
 embed = hub.KerasLayer(hub_url)
 model = tf.keras.Sequential([
     embed
 ])
 
+# build model
 model.build([None, SIZE, SIZE, 3])  # Batch input shape.
 model.summary()
 
+# read classes
+classes = [line.rstrip('\n') for line in open(CLASSES_FILENAME)]
+
+
+# read test image and vectorize it
 file_path = '/Users/Anna/Explainator/explainator/data/panda.jpg'
 
 img = tf.io.read_file(file_path)
@@ -22,9 +31,11 @@ images = tf.cast(images, tf.float32) / 128.
 images.set_shape((None, None, None, 3))
 test_image = tf.image.resize(images, (SIZE, SIZE))
 
-
+# get model predictions on image
 predictions = model.predict(test_image)
+class_index = np.argmax(predictions[0])
 
-pred = np.argmax(predictions[0])
+image_class = classes[class_index]
 
-print(pred)
+print('Predicted class: {}. Score: {} '.format(image_class, np.max(predictions[0])))
+
