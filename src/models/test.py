f232046fc154f5e5c3b85389ad5ca045d2d266d1
Anna <anna.belowska2@gmail.com>
2019-11-03 23:21:55 +0100

    Minor refactor

--- src/models/test.py
+++ src/models/test.py
@@ -3,7 +3,9 @@ import tensorflow_hub as hub
 import numpy as np
 
 SIZE = 224
-CLASSES_FILENAME = '/Users/Anna/Explainator/explainator/data/classes.txt'
+CLASSES_PATH = '/Users/Anna/Explainator/explainator/src/models/imagenet_classes.txt'
+TEST_IMAGE_PATH = '/Users/Anna/Explainator/explainator/data/panda.jpg'
+
 
 # download construct model
 hub_url = "https://tfhub.dev/google/imagenet/mobilenet_v2_035_224/classification/4"
@@ -17,19 +19,17 @@ model.build([None, SIZE, SIZE, 3])  # Batch input shape.
 model.summary()
 
 # read classes
-classes = [line.rstrip('\n') for line in open(CLASSES_FILENAME)]
+classes = [line.rstrip('\n') for line in open(CLASSES_PATH)]
 
 
 # read test image and vectorize it
-file_path = '/Users/Anna/Explainator/explainator/data/panda.jpg'
-
-img = tf.io.read_file(file_path)
+img = tf.io.read_file(TEST_IMAGE_PATH)
 img = tf.image.decode_jpeg(img)
 
-images = tf.expand_dims(img, 0)
-images = tf.cast(images, tf.float32) / 128.
-images.set_shape((None, None, None, 3))
-test_image = tf.image.resize(images, (SIZE, SIZE))
+img = tf.expand_dims(img, 0)
+img = tf.cast(img, tf.float32) / 128.
+img.set_shape((None, None, None, 3))
+test_image = tf.image.resize(img, (SIZE, SIZE))
 
 # get model predictions on image
 predictions = model.predict(test_image)
@@ -37,5 +37,5 @@ class_index = np.argmax(predictions[0])
 
 image_class = classes[class_index]
 
-print('Predicted class: {}. Score: {} '.format(image_class, np.max(predictions[0])))
-
+print('Predicted class: {}. Score: {} in score range: ({} , {})'.format(image_class, np.max(predictions[0]), np.min(predictions[0]), np.max(predictions[0])))
+# print(class_index)
