6a172f56626f0bd30eb51fcfa1d72827a0db788c
Anna <anna.belowska2@gmail.com>
2019-11-04 00:57:46 +0100

    Don't specify python3's version

--- nix.sh
+++ nix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install virtualenv
 pip install jupyterlab
-virtualenv -p python3.7 venv
+virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
 python -m ipykernel install --user --name=explainator-py
