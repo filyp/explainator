20c6147e5310e07c10254462696940dbe668d230
fsondej <filipsondej@protonmail.com>
2019-11-11 16:03:26 +0100

    fix pip install location make requirements consistent

--- nix.sh
+++ nix.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-pip install virtualenv
-pip install jupyterlab
+pip install --user virtualenv
+pip install --user jupyterlab
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
