f973c4dfbcf6d58dd8aa2c7b41ca763676b89b60
AgnLiska <agiusia@gmail.com>
2019-11-20 21:45:38 +0100

    nix update from virtualenv to venv

--- nix.sh
+++ nix.sh
@@ -3,7 +3,7 @@ set -e
 
 pip install --user virtualenv
 pip install --user jupyterlab
-virtualenv -p python3 venv
-source venv/bin/activate
+python3 -m venv .
+source ./bin/activate
 pip install -r requirements.txt
 python -m ipykernel install --user --name=explainator-py
